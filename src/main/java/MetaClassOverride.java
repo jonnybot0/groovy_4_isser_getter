25b86c5a4e75d4e94b48e1783d3dccc28815a8ef
Jonny Carter <jcarter@adaptavist.com>
2022-12-01 11:50:51 -0600

    Replace isser with getter

--- src/main/java/MetaClassOverride.java
+++ src/main/java/MetaClassOverride.java
@@ -1,6 +1,12 @@
-import groovy.lang.MetaClassImpl;
-import groovy.lang.MetaClassRegistry;
-import groovy.lang.MetaMethod;
+import groovy.lang.*;
+import org.codehaus.groovy.reflection.CachedClass;
+import org.codehaus.groovy.reflection.CachedMethod;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.*;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 public class MetaClassOverride extends MetaClassImpl {
     public MetaClassOverride(Class theClass, MetaMethod[] add) {
@@ -22,7 +28,34 @@ public class MetaClassOverride extends MetaClassImpl {
     @Override
     protected synchronized void reinitialize() {
         super.reinitialize();
-        System.out.println("I am in your compiler, overriding your MetaClass.");
-        setInitialized(true);
+        try {
+            Field propertyIndexField = MetaClassImpl.class.getDeclaredField("classPropertyIndex");
+            propertyIndexField.setAccessible(true);
+            Map<CachedClass, LinkedHashMap<String, MetaProperty>> classPropertyIndex =
+                    (Map<CachedClass, LinkedHashMap<String, MetaProperty>>) propertyIndexField.get(this);
+            LinkedHashMap<String, MetaProperty> propertiesForCachedClass = classPropertyIndex.get(theCachedClass);
+            Set<Map.Entry<String, MetaProperty>> entries = propertiesForCachedClass.entrySet();
+            for (Map.Entry<String, MetaProperty> entry : entries) {
+                String propertyName = entry.getKey();
+                String capitalizedPropertyName = propertyName.substring(0, 1).toUpperCase() + propertyName.substring(1);
+                MetaProperty metaProp = entry.getValue();
+                if (metaProp instanceof MetaBeanProperty) {
+                    Stream<Method> methodStream = Arrays.stream(theClass.getMethods());
+                    Predicate<Method> getterFinder = method -> method.getName().equals("get" + capitalizedPropertyName);
+                    Optional<Method> optionalMethod = methodStream.filter(getterFinder).findFirst();
+                    if (metaProp.getType().getName().equals("boolean") && optionalMethod.isPresent()) {
+                        Method getterMethod = optionalMethod.get();
+                        addMetaBeanProperty(
+                                new MetaBeanProperty(propertyName,
+                                        metaProp.getType(),
+                                        CachedMethod.find(getterMethod),
+                                        ((MetaBeanProperty) metaProp).getSetter())
+                        );
+                    }
+                }
+            }
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
