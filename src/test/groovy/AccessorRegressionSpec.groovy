c96091efc2ec3fb450efab8df05a5f8798d824e4
Jonny Carter <jcarter@adaptavist.com>
2022-12-01 10:02:43 -0600

    Add custom external java lib and use that for test

--- src/test/groovy/AccessorRegressionSpec.groovy
+++ src/test/groovy/AccessorRegressionSpec.groovy
@@ -1,8 +1,10 @@
 import spock.lang.Specification;
 
+import com.adaptavist.ClassWithConflictingProp
+
 class AccessorRegressionSpec extends Specification {
     def "getter is preferred for accessors when there is an isser and non-boolean getter"() {
         expect:
-        new ClassWithConflictingAccessors().conflictingProperty == "getter"
+        new ClassWithConflictingProp().conflictingProperty == "getter"
     }
 }
