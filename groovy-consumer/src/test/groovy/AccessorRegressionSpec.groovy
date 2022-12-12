a9b30cffe295441a910cca9d092186fb7d22a1ae
Jonny Carter <jcarter@adaptavist.com>
2022-12-12 13:08:47 -0600

    Refactor static test so I can fiddle with the classloader

--- groovy-consumer/src/test/groovy/AccessorRegressionSpec.groovy
+++ groovy-consumer/src/test/groovy/AccessorRegressionSpec.groovy
@@ -5,11 +5,16 @@ class AccessorRegressionSpec extends Specification {
 
     def "use a static class"() {
         when:
-        new StaticallyCompiledConsumerClass().useAmbiguousMethods()
+        def groovyClassLoader = new GroovyClassLoader()
+        def staticallyCompiledClass = groovyClassLoader.parseClass(
+            this.class.classLoader.getResource('StaticallyCompiledConsumerClass.groovy').text
+        )
+        staticallyCompiledClass.newInstance().useAmbiguousMethods()
 
         then:
         notThrown(Exception)
     }
+
     def "getter is preferred for accessors when there is an isser and non-boolean getter"() {
         setup:
         GroovySystem.getMetaClassRegistry().setMetaClassCreationHandle(new FunkyMetaClassCreationHandle())
