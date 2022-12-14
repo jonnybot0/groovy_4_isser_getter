d067a8388e3282d302e57d2fb6c79207169c87ad
Jonny Carter <jcarter@adaptavist.com>
2022-12-14 15:51:28 -0600

    Try setProperty... no dice

--- groovy-consumer/src/main/resources/FunkyPrecompiledTypeCheckingExtension.groovy
+++ groovy-consumer/src/main/resources/FunkyPrecompiledTypeCheckingExtension.groovy
@@ -2,7 +2,6 @@ import org.codehaus.groovy.ast.MethodNode
 import org.codehaus.groovy.ast.Parameter
 import org.codehaus.groovy.ast.expr.PropertyExpression
 import org.codehaus.groovy.transform.stc.GroovyTypeCheckingExtensionSupport
-import org.codehaus.groovy.transform.stc.StaticTypesMarker
 
 class FunkyPrecompiledTypeCheckingExtension extends GroovyTypeCheckingExtensionSupport.TypeCheckingDSL {
     @Override
@@ -11,13 +10,14 @@ class FunkyPrecompiledTypeCheckingExtension extends GroovyTypeCheckingExtensionS
             if (expression instanceof PropertyExpression) {
                 if (node instanceof MethodNode) {
                     def methodName = node.name
-                    def getterName = "get" + (node.name - "is")
+                    def propertyName = node.name - "is"
+                    def lowerPropertyName = propertyName[0].toLowerCase() + propertyName[1..-1]
+                    def getterName = "get" + propertyName
                     def hasGetter = node.getDeclaringClass().hasDeclaredMethod(getterName, new Parameter[]{})
                     if (node.returnType.name == "boolean" && methodName.startsWith("is") && hasGetter) {
                         def getter = node.getDeclaringClass().getDeclaredMethod(getterName, new Parameter[]{})
                         println "Class has a getter and an isser for this property. Time to transform it!"
-                        expression.metaDataMap[StaticTypesMarker.INFERRED_TYPE] = getter.returnType
-                        expression.metaDataMap[StaticTypesMarker.DIRECT_METHOD_CALL_TARGET] = getter
+                        setProperty(lowerPropertyName, getter)
                     }
                 }
             }
