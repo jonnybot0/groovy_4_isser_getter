9197f45f99c6dce677c968ffcd03992a0a7ec70e
Jonny Carter <jcarter@adaptavist.com>
2022-12-15 11:46:44 -0600

    WIP - looks like only onMethodSelection lets me touch the externally defined lib... while I can add properties to a class, it seems that onMethodSelection happens before properties have been assigned to the class... or perhaps simply adding a new property doesn't matter at this point

--- groovy-consumer/src/main/groovy/FunkyPrecompiledTypeCheckingExtension.groovy
+++ groovy-consumer/src/main/groovy/FunkyPrecompiledTypeCheckingExtension.groovy
@@ -1,11 +1,17 @@
+import groovyjarjarasm.asm.Opcodes
 import org.codehaus.groovy.ast.ClassNode
+import org.codehaus.groovy.ast.MethodNode
 import org.codehaus.groovy.ast.Parameter
 import org.codehaus.groovy.ast.PropertyNode
+import org.codehaus.groovy.ast.expr.Expression
+import org.codehaus.groovy.ast.expr.ExpressionTransformer
+import org.codehaus.groovy.ast.expr.PropertyExpression
 import org.codehaus.groovy.transform.stc.GroovyTypeCheckingExtensionSupport
 
 class FunkyPrecompiledTypeCheckingExtension extends GroovyTypeCheckingExtensionSupport.TypeCheckingDSL {
     @Override
     Object run() {
+/*
         afterVisitClass { ClassNode classNode ->
             println "Class visisted ${classNode}"
             classNode.properties.findAll { PropertyNode propertyNode ->
@@ -20,6 +26,50 @@ class FunkyPrecompiledTypeCheckingExtension extends GroovyTypeCheckingExtensionS
                         propertyNode.getterBlock = getter.code
                         propertyNode.getterName = getter.name
                         classNode.addProperty(propertyNode)
+                    }
+                }
+            }
+        }
+*/
+/*
+        //Nothing here
+        unresolvedProperty { PropertyExpression pexp ->
+            println "Found unresolved property expresion $pexp"
+        }
+*/
+/*
+
+        // Finished checking method call org.codehaus.groovy.ast.expr.ConstructorCallExpression@5d8445d7[type: com.adaptavist.ClassWithConflictingProp -> com.adaptavist.ClassWithConflictingProp arguments: org.codehaus.groovy.ast.expr.ArgumentListExpression@37d80fe7[]]
+        afterMethodCall { call ->
+            println "Finished checking method call ${call}"
+        }
+*/
+        onMethodSelection { expression, node ->
+            println "Method selection: ${expression} ${node}"
+            if (expression instanceof PropertyExpression) {
+                if (node instanceof MethodNode) {
+                    def methodName = node.name
+                    def propertyName = node.name - "is"
+                    def lowerPropertyName = propertyName[0].toLowerCase() + propertyName[1..-1]
+                    def getterName = "get" + propertyName
+                    def hasGetter = node.getDeclaringClass().hasDeclaredMethod(getterName, new Parameter[]{})
+                    if (node.returnType.name == "boolean" && methodName.startsWith("is") && hasGetter) {
+                        def getter = node.getDeclaringClass().getDeclaredMethod(getterName, new Parameter[]{})
+                        println "Class has a getter and an isser for this property ($lowerPropertyName). Time to transform it!"
+                        def declaringClass = node.declaringClass
+                        def extantProperty = declaringClass.hasProperty(lowerPropertyName) ? declaringClass.getProperty(lowerPropertyName) : null
+                        println "Declaring class props before: ${declaringClass.getProperties()}"
+                        declaringClass.addProperty(
+                            new PropertyNode(
+                                lowerPropertyName,
+                                Opcodes.ACC_PUBLIC,
+                                getter.returnType, declaringClass,
+                                null, //TODO: need some way to calc initial value expression
+                                getter.code,
+                                extantProperty?.setterBlock
+                            )
+                        )
+                        println "Declaring class props after: ${declaringClass.getProperties()}"
                     }
                 }
             }
